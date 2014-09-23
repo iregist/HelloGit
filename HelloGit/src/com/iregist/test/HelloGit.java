a2f268fee54d4d2aa66fa4a5778fad1f2efb5589
ibic <ibic.com@gmail.com>
2014-09-23 22:41:03 +0800

    Initial checkin

---
+++ HelloGit/src/com/iregist/test/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.iregist.test;
+
+/**
+ * @author Jeff
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git!");
+	}
+
+}
